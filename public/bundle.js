c2ff4b09efc126b1dcd0326ae029c213ca00045c
tuningmind <tuningmind@gmail.com>
2017-03-31 17:46:16 -0600

    Remove unused code

--- public/bundle.js
+++ public/bundle.js
@@ -70,84 +70,7 @@
 /* 0 */
 /***/ (function(module, exports) {
 
-const calculator = (function () {
-  
-  const getOps = function(buttonValue) {
-  // takes an empty array and an operation
-  // returns object containing operands, operator, and calculated result
-    const ops = {
-      operands: [],
-      operator: "",
-      calculated: ""
-    }
-    if (buttonValue === "C") { 
-      ops.operands = [];
-      ops.operator = "";
-    }
-    else if (typeof (buttonValue) === "number") { 
-      ops.operands.push(parseInt(buttonValue)) 
-    }
-    else if (typeof (buttonValue === "number")) { 
-      ops.operator = buttonValue;
-    }
-    else if (buttonValue === "=") {
-      ops.calculated = calculate(ops.operands, ops.operator)
-    }
-    return ops 
-  }
-
-  const calculate = function(ops) {
-  // takes object containing an array of numbers and an operation
-  // returns a number that is the result of calling the operation on the array's numbers 
-    function add(arr) { arr[0] + arr[1] }
-    function subtract(arr) { arr[0] - arr[1] }
-    function multiply(arr) { arr[0] * arr[1] }
-    function divide(arr) { arr[0] / arr[1] }
-    const operators = {
-      "+": add,
-      "-": subtract,
-      "×": multiply,
-      "÷": divide,
-    }
-    return operators[ops.operator](ops.operand)
-  }
-
-  const getClickValue = function(nodelist) {
-  // takes a nodeList
-  // creates an event listener for each node in the list
-  // returns the value collected by clicking on one of those nodes
-    for (let button of nodelist) {
-      button.addEventListener("click", function () {
-        return button.value
-      })
-    }
-  }
-
-  const printToScreen = function(buttonValue) {
-  // takes a button value
-  // adds the button value to the screen node
-    screen.textContent += button.value
-  }
-
-  //const buttonlist = document.querySelectorAll("input[type='button']");
-  //const screen = document.querySelector("#screen")
-
-  //const buttonValue = getClickValue(buttonlist)
-  //const ops = getOps(buttonValue)
-  //const calculated = calculate(ops)
-  //const
-  const log = function() {
-    return console.log("build worked")
-  }
-
-  return {
-    log: log,
-    getOps: getOps
-  }
-})()
-
-module.exports = calculator
-
+throw new Error("Module parse failed: /Users/tuningmind/dev/ironyard/calculator-wk-03/scripts/calculator.js Unexpected token (10:4)\nYou may need an appropriate loader to handle this file type.\n|   const opfunctions = {\n|     \"+\": function (operands) { return operands[0] + operands[1]}\n|     \"-\": function (operands) { return operands[0] - operands[1] },\n|     \"×\": function (operands) { return operands[0] * operands[1] },\n|     \"÷\": function (operands) { return operands[0] / operands[1] }");
 
 /***/ }),
 /* 1 */
@@ -156,7 +79,8 @@ module.exports = calculator
 const calculator = __webpack_require__(0)
 
 calculator.log()
-console.log(calculator.getOps(3))
+calculator.updateOps("=")
+console.log(ops)
 
 
 /***/ })
--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,10 +1,29 @@
-const calculator = (function () {
+;const calculator = (function () {
 
-  const ops = {
-    operands: [],
-    operator: "",
-    calculated: ""
+  const buttonlist  = document.querySelectorAll("input[type='button']");
+  const screen      = document.querySelector("#screen")
+  const ops         = {
+                         operands: [],
+                         operator: ""
+                      }
+  const opfunctions = {
+    "+": function (operands) { return operands[0] + operands[1] },
+    "-": function (operands) { return operands[0] - operands[1] },
+    "×": function (operands) { return operands[0] * operands[1] },
+    "÷": function (operands) { return operands[0] / operands[1] }
+                      }
+
+
+  const makeAnswer = function (opfunction, operands) {
+    operands = ops.operands.map(Number)
+    console.log(operands)
+    const calculated = opfunctions[opfunction](operands)
+    return calculated
   }
+
+  let opfunction = opfunctions[ops.operator]
+  let operands = ops.operands
+  const answer = makeAnswer(opfunction, operands)
   
   const updateOps = function(buttonValue) {
   // takes an empty array and an operation
@@ -17,7 +36,7 @@ const calculator = (function () {
       ops.operands.push(parseInt(buttonValue)) 
     }
     else if (buttonValue === "=") {
-      ops.calculated = calculate(ops.operands, ops.operator)
+      ops.calculated = makeAnswer(ops.operands, ops.operator)
     }
     else if (["+","-","×","÷"].indexOf(buttonValue ) > 0) { 
       ops.operator = buttonValue;
@@ -25,32 +44,17 @@ const calculator = (function () {
     return ops 
   }
 
-  const calculate = function(ops) {
-  // takes object containing an array of numbers and an operation
-  // returns a number that is the result of calling the operation on the array's numbers 
-    const add = function (arr) { arr[0] + arr[1] }
-    const subtract = function (arr) { arr[0] - arr[1] }
-    const multiply = function (arr) { arr[0] * arr[1] }
-    const divide = function (arr) { arr[0] / arr[1] }
-    const operators = {
-      "+": add,
-      "-": subtract,
-      "×": multiply,
-      "÷": divide,
-    }
-    return operators[ops.operator](ops.operand)
-  }
-
   const getClickValue = function(nodelist) {
-  // takes a nodeList
-  // creates an event listener for each node in the list
-  // returns the value collected by clicking on one of those nodes
+  /* takes a nodeList
+  creates an event listener for each node in the list
+  returns the value collected by clicking on one of those nodes */
     for (let button of nodelist) {
       button.addEventListener("click", function () {
         return button.value
       })
     }
   }
+  const buttonValue = getClickValue(buttonlist)
 
   const printToScreen = function(buttonValue) {
   // takes a button value
@@ -58,10 +62,7 @@ const calculator = (function () {
     screen.textContent += button.value
   }
 
-  //const buttonlist = document.querySelectorAll("input[type='button']");
-  //const screen = document.querySelector("#screen")
 
-  //const buttonValue = getClickValue(buttonlist)
   //const ops = getOps(buttonValue)
   //const calculated = calculate(ops)
   //const
@@ -80,10 +81,3 @@ const calculator = (function () {
 
 module.exports = calculator
 
-//const operator = function(arr) {arr[0] + arr[1]}
-//const operand = ["1", "2"]
-//
-//const calculate = function(operator, operand) { 
-//  return operator(operand)
-//  }
-//  calculate(operator(operand))
