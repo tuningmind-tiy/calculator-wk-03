30a894e5ec02bfdaae567d32f0bf9daf2f6703aa
tuningmind <tuningmind@gmail.com>
2017-04-02 13:02:25 -0600

    Troubleshooting getClickValue function

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,28 +1,54 @@
-window.onload = (function() {
+const calculator = ( function() {
 
-  const screen        = document.querySelector("#screen")
+  const screen        = document.querySelector("div#screen")
   const buttonlist    = document.querySelectorAll("input[type='button']")
+  // const ops           = {
+  //   operands: [],
+  //   operator: '',
+  //   answer: ''
+  // } 
+  // const isEqualSign   = function( value ) { return value === "=" }
+  // const isOperator    = function( operatorString ) {
+  //   return ["+","-","×","÷"].indexOf( operatorString ) >= 0 
+  // }
+  // const isNumber      = function( buttonValue ) {
+  //   buttonVal = Number(buttonValue)
+  //   return ( typeof( buttonVal ) === 'number' )
+  // }
+  // const opfunctions   = {
+  //   "+": function( operands ) { return operands[0] + operands[1]},
+  //   "-": function( operands ) { return operands[0] - operands[1]},
+  //   "×": function( operands ) { return operands[0] * operands[1]},
+  //   "÷": function( operands ) { return operands[0] / operands[1]}
+  // }
+  // const makeAnswer    = function( opfunction, operands ) {
+  //   operands = operands.map(Number)
+  //   const calculated = opfunctions[opfunction](operands)
+  //   return calculated
+  // }
 
-  const updateOps = function(buttonValue) {
-    const ops           = {
-      operands: [],
-      operator: ''
-    } 
-    const buttonValues = {
-      "C":  
-    }
-
-  }
-
-  const getClickValue = function () {
-    
-    for (let button of buttonlist) {
-      button.addEventListener("click", function() {
-        updateOps(button.value)
-        screen.textContent = button.value
-      }) // end anonymous function
-    } // end for loop
-  } // end getclickvalue function
+  // const updateOps = function( buttonValue ) {
+  //   if ( buttonValue === "C" ) {
+  //     ops.operands = [],
+  //     ops.operator = ''
+  //   } else if ( isOperator( buttonValue )) {
+  //     ops.operator = buttonValue
+  //   } else if ( isNumber( buttonValue )) {
+  //     ops.operands.push( buttonValue )
+  //   } else if ( isEqualSign( buttonValue )) {
+  //     ops.answer = makeAnswer( ops.operands, ops.operator )
+  //   }
+  // }
+ let getClickValue = function () {
+   console.log("clicked")
+   for ( var button of buttonlist ) {
+     console.log(button)
+     button.addEventListener( "click", function() {
+     screen.textContent = button.value
+     }) // end anonymous function
+   } // end for loop
+ } // end getclickvalue function
   
+})() // end window.onload
+window.onload = calculator;
 
-}) // end window.onload
