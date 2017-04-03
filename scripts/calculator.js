7ffce285ea18aaf31199aaacf6f662005fe57f8c
tuningmind <tuningmind@gmail.com>
2017-04-02 20:14:55 -0600

    Working state

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,51 +1,51 @@
 const calculator = ( function() {
 
+  const ops           = {
+    operands: [],
+    operator: '',
+    answer: ''
+  } 
+  const isEqualSign   = function( value ) { return value == "=" }
+  const isOperator    = function( operatorString ) {
+    return ["+","-","×","÷"].indexOf( operatorString ) >= 0 
+  }
+  const isNumber      = function( buttonValue ) {
+    buttonVal = Number(buttonValue)
+    return ( !isNaN( buttonVal ) )
+  }
+  const opfunctions   = {
+    "+": function( operands ) { return operands[0] + operands[1]},
+    "-": function( operands ) { return operands[0] - operands[1]},
+    "×": function( operands ) { return operands[0] * operands[1]},
+    "÷": function( operands ) { return operands[0] / operands[1]}
+  }
+  const makeAnswer    = function( opfunction, operands ) {
+    operands = operands.map(Number)
+    const calculated = opfunctions[opfunction](operands)
+    return calculated
+  }
+
+  const updateOps = function( buttonValue ) {
+    if ( buttonValue === "C" ) {
+      ops.operands = [],
+      ops.operator = ''
+    } else if ( isOperator( buttonValue )) {
+      ops.operator = buttonValue
+    } else if ( isNumber( buttonValue )) {
+      ops.operands.push( buttonValue )
+    } else if ( isEqualSign( buttonValue )) {
+      ops.answer = makeAnswer( ops.operator, ops.operands)
+    }
+  }
   const screen        = document.querySelector("div#screen")
   const buttonlist    = document.querySelectorAll("input[type='button']")
-  // const ops           = {
-  //   operands: [],
-  //   operator: '',
-  //   answer: ''
-  // } 
-  // const isEqualSign   = function( value ) { return value === "=" }
-  // const isOperator    = function( operatorString ) {
-  //   return ["+","-","×","÷"].indexOf( operatorString ) >= 0 
-  // }
-  // const isNumber      = function( buttonValue ) {
-  //   buttonVal = Number(buttonValue)
-  //   return ( typeof( buttonVal ) === 'number' )
-  // }
-  // const opfunctions   = {
-  //   "+": function( operands ) { return operands[0] + operands[1]},
-  //   "-": function( operands ) { return operands[0] - operands[1]},
-  //   "×": function( operands ) { return operands[0] * operands[1]},
-  //   "÷": function( operands ) { return operands[0] / operands[1]}
-  // }
-  // const makeAnswer    = function( opfunction, operands ) {
-  //   operands = operands.map(Number)
-  //   const calculated = opfunctions[opfunction](operands)
-  //   return calculated
-  // }
-
-  // const updateOps = function( buttonValue ) {
-  //   if ( buttonValue === "C" ) {
-  //     ops.operands = [],
-  //     ops.operator = ''
-  //   } else if ( isOperator( buttonValue )) {
-  //     ops.operator = buttonValue
-  //   } else if ( isNumber( buttonValue )) {
-  //     ops.operands.push( buttonValue )
-  //   } else if ( isEqualSign( buttonValue )) {
-  //     ops.answer = makeAnswer( ops.operands, ops.operator )
-  //   }
-  // }
-  function getClickValue( nodelist ) {
-    for ( var button of buttonlist ) {
-      button.addEventListener( "click", function() {
-        console.log(button.value)
-      return button.value
-      }) 
-    } 
+  for ( let button of buttonlist) {
+    button.addEventListener( "click", function() {
+      printToScreen( button.value )
+      updateOps( button.value )
+      console.log("ops: ", ops)
+      if (isEqualSign( button.value )) { printToScreen( ops.answer ) }
+    }) 
   } 
 
   const printToScreen = function( buttonValue ) {
@@ -55,15 +55,9 @@ const calculator = ( function() {
   return {
     buttonlist:     buttonlist, 
     screen:         screen,
-    getClickValue:  getClickValue,
     printToScreen:  printToScreen
   }
   
 })() 
 
-window.onload = calculator
-debugger
-let buttonValue = calculator.getClickValue( calculator.buttonlist )
-calculator.printToScreen( buttonValue )
-console.log("buttonValue : ", buttonValue )
 
